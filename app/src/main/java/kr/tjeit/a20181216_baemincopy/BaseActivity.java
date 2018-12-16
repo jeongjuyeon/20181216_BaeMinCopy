5135f416b8430e003f38683361d3b0c42eb630bb
HanulGye <kyehl@naver.com>
2018-12-16 13:38:14 +0900

    메인액티비티 xml 수정 / 자바코드 + 베이스액티비티 생성

--- app/src/main/java/kr/tjeit/a20181216_baemincopy/BaseActivity.java
+++ app/src/main/java/kr/tjeit/a20181216_baemincopy/BaseActivity.java
@@ -4,7 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
-    public abstract void setupEvents():
-    public abstract void setValues():
+    public abstract void setupEvents();
+    public abstract void setValues();
     public abstract void bindViews();
+
 }
